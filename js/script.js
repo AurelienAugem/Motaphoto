96343f517343792d268128eecd6137c540e4a248
AurelienAugem <130144174+AurelienAugem@users.noreply.github.com>
2023-07-20 10:00:54 +0200

    amelioration form

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 let formulaire = document.querySelector('.formulaire-contact');
 let contact = document.querySelector('#menu-item-26 a');
-formulaire.classList.add('hide');
+formulaire.classList.add('hidden');
 
 //Affichage du formulaire de contact
 function showForm() {
-    if(formulaire.classList.contains("hide")){
+    if(formulaire.classList.contains("hidden")){
         formulaire.classList.add('form-animation');
         formulaire.classList.add('show');
-        formulaire.classList.remove('hide');
+        formulaire.classList.remove('hidden');
     }else if(formulaire.classList.contains("show")){
         formulaire.classList.remove('form-animation');
         formulaire.classList.add('hide');
+        formulaire.classList.add('hidden');
         formulaire.classList.remove('show');
     }  
 }
