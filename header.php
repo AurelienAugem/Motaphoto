7e6e2ddf26db96baefd27ca575274f83edce619e
AurelienAugem <130144174+AurelienAugem@users.noreply.github.com>
2023-09-13 11:19:25 +0200

    supp

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Motaphoto</title>
+    <meta name="description" content="Site de la photographe professionnelle d'evenementiel Nathalie Mota"> 
     <?php wp_head() ?>
 </head>
 
